395661fa02a36ef51bb472e759e9006e439404c5
NguyenThuong-code <you@example.com>
2022-10-30 09:09:43 +0700

    Shoppong Cart

---
+++ src/main/java/com/thuong/repository/IProductRepository.java
@@ -0,0 +1,9 @@
+package com.thuong.repository;
+
+import com.thuong.model.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductRepository extends PagingAndSortingRepository<Product,Long> {
+}
